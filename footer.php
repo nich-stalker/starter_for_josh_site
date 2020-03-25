d734e4af8cd51b87a804604415b5a60d32992cec
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-03-26 07:46:50 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,16 +4,16 @@
 
                 <div class="col-md-6 text-center footer-content">
                     <p><?php bloginfo('name'); ?></p>
-                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr”>
-                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt=“rss”>
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/themes/starter_for_josh_site/images/tumblr.jpg" alt=“tumblr”>
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/themes/starter_for_josh_site/images/rss.jpg" alt=“rss”>
 
                 </div>
                 <!--col-md-6-->
 
                 <div class="col-md-6 text-center footer-content">
                     <p>Terms and Conditions</p>
-                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished//images/linkedin.jpg" alt=“linkedin”>
-                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/facebook.jpg" alt=“facebook”>
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/themes/starter_for_josh_site/images/linkedin.jpg" alt=“linkedin”>
+                    <img src="http://206.189.45.97/~mesh21/sub/wp-content/themes/starter_for_josh_site/images/facebook.jpg" alt=“facebook”>
 
                 </div>
                 <!--col-md-6-->
