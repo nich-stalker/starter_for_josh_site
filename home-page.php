9c6e7c5795b3fb7863760ab25dc78bca5a3715ac
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-04-02 16:18:18 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,44 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>   
+<section class="container-fluid aboutbg text-center">
+
+        <div class="container">
+
+           
+            <div class="blocker"></div>
+        </div>
+        <!--container-->
+    </section>
+
+
+    <section class="container-fluid articlesbg">
+        <div class="container">
+            <div class="row">
+                <div class="article-title text-center">
+                    <h3>LATEST ARTICLES</h3>
+                </div>
+<div class="col-md-4 article-cent">
+<!--placeholder for col1-->
+                </div>
+                <!--col-md-4-->
+
+                <div class="col-md-4 article-cent">
+<!--placeholder for col2-->
+                </div>
+                <!--col-md-4-->
+
+                <div class="col-md-4 article-cent">
+<!--placeholder for col3-->
+                </div>
+                <!--col-md-4-->
+
+            </div>
+            <!--row-->
+        </div>
+        <!--container-->
+    </section>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
